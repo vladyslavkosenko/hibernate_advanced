3bb96192fd8c47dcea1c9375ad25e138d7e6beb1
vladyslavkosenko <vaduha91@gmail.com>
2022-09-03 14:11:06 +0300

    first commit

---
+++ src/main/java/com/example/hibernate_advanced/HibernateAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAdvancedApplication.class, args);
+	}
+
+}
